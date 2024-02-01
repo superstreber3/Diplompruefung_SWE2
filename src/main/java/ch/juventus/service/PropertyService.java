ebb0b783db3592d8d2e9a6663cb8579b51080c78
Silas Zahner <silas.zahner@gmail.com>
2024-02-01 19:14:23 +0100

    added sorting functions for apartments

--- src/main/java/ch/juventus/service/PropertyService.java
+++ src/main/java/ch/juventus/service/PropertyService.java
@@ -4,8 +4,9 @@ import ch.juventus.database.Database;
 import ch.juventus.model.Apartment;
 import ch.juventus.model.Property;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PropertyService {
 
@@ -19,16 +20,28 @@ public class PropertyService {
         return database.getAllProperties();
     }
 
+    // a) Liste aller freien Apartments
     public List<Apartment> getAllFreeApartments() {
-        return Collections.emptyList();
+        return database.getAllProperties().stream()
+                .flatMap(property -> property.getApartments().stream())
+                .filter(apartment -> !apartment.isRented())
+                .collect(Collectors.toList());
     }
 
+    // b) Liste aller Properties in einer Stadt
     public List<Property> getAllPropertiesInCity(String city) {
-        return Collections.emptyList();
+        return database.getAllProperties().stream()
+                .filter(property -> property.getAddress().getCity().equalsIgnoreCase(city))
+                .collect(Collectors.toList());
     }
 
+    // c) Liste aller Apartments, sortiert nach der Anzahl der Zimmer und Vermietungsstatus
     public List<Apartment> getAllApartmentsSortedByNumberOfRooms() {
-        return Collections.emptyList();
+        return database.getAllProperties().stream()
+                .flatMap(property -> property.getApartments().stream())
+                .sorted(Comparator.comparingInt((Apartment a) -> a.getRooms().values().stream().reduce(0, Integer::sum))
+                        .thenComparing(Apartment::isRented))
+                .collect(Collectors.toList());
     }
 
 }
